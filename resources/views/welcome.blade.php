fba74e2e7f9951ee0a0a1b9c01f6add6524c4ff6
Lucas Martins <lucasamtb02@gmail.com>
2023-08-26 16:55:55 -0300

    Implementação da Busca

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,16 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method="GET">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
-    <h2>Proximos Eventos</h2>
+    @if($search)
+        <h2>Resultado de busca por: {{$search}}</h2>
+    @else
+        <h2>Proximos Eventos</h2>
+    @endif
     <p class="subtitle">Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
         @foreach($events as $event)
@@ -19,14 +23,16 @@
                 <img src="./img/events/{{$event->image}}" alt="{{$event->title}}">
                 <div class="card-body">
                     <p class="card-title">{{$event->title}}</p>
-                    <p class="card-date">{{date('d/m/y', strtotime($event->date))}}</p>
+                    <p class="card-date">{{date('d/m/ynt->tit', strtotime($event->date))}}</p>
                     <p class="card-participants">X Participantes</p>
                     <a href="/events/{{$event->id}}" class="btn btn-primary">Veja mais</a>
                 </div>
             </div>
         @endforeach
-        @if(count($events) == 0)
-            <p class="badge badge-secondary">Não há eventos disponíveis</p>
+        @if(count($events) == 0 && $search)
+            <p class="badge badge-secondary">Não foi possível encontrar nenhum evento por: {{$search}}! <a href="/">Ver todos</a></p>
+            @elseif(count($events) == 0)
+                <p>Não hpa eventos disponíveis</p>
         @endif
     </div>
 </div>
